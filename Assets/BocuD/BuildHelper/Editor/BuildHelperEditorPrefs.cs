27e07a01589c565c5d369eab78b4ca40f7ae8316
BocuD <duco.born@gmail.com>
2022-04-08 16:26:18 +0200

    Remove unused autosave setting

--- Assets/BocuD/BuildHelper/Editor/BuildHelperEditorPrefs.cs
+++ Assets/BocuD/BuildHelper/Editor/BuildHelperEditorPrefs.cs
@@ -5,32 +5,17 @@ namespace BocuD.BuildHelper
     [InitializeOnLoad]
     public static class BuildHelperEditorPrefs
     {
-        private const string autoSavePath = "BuildHelperAutoSave";
         private const string asyncPublishPath = "BuildHelperAsyncPublish";
         private const string buildNumberPath = "BuildHelperNumberMode";
         private const string platformSwitchPath = "BuildHelperNumberMode";
         
         static BuildHelperEditorPrefs()
         {
-            _autoSave = EditorPrefs.GetBool(autoSavePath);
             _useAsyncPublish = EditorPrefs.GetBool(asyncPublishPath);
             _buildNumberMode = EditorPrefs.GetInt(buildNumberPath);
             _platformSwitchMode = EditorPrefs.GetInt(platformSwitchPath);
         }
-
-        private static bool _autoSave;
-        public static bool AutoSave
-        {
-            set
-            {
-                if (_autoSave == value) return;
-                
-                _autoSave = value;
-                EditorPrefs.SetBool(autoSavePath, value);
-            }
-            get => _autoSave;
-        }
-
+        
         private static bool _useAsyncPublish;
         public static bool UseAsyncPublish
         {
