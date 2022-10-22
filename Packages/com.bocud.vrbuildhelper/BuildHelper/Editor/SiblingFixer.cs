d38bd98e33dcb380a4e084cea2400eae0c6120cb
BocuD <duco.born@gmail.com>
2022-10-23 00:20:27 +0200

    Add an option to fix sibling transforms in selected gameobject

--- Packages/com.bocud.vrbuildhelper/BuildHelper/Editor/SiblingFixer.cs
+++ Packages/com.bocud.vrbuildhelper/BuildHelper/Editor/SiblingFixer.cs
@@ -11,21 +11,40 @@ namespace BocuD.BuildHelper.Editor
     public static class SiblingFixer
     {
         //todo run this on vrc build callback
-        [MenuItem("Tools/Build Helper/Fix sibling transforms")]
+        [MenuItem("Tools/Build Helper/Fix scene sibling transforms")]
         public static void FixSiblings()
         {
             bool doit = EditorUtility.DisplayDialog("VRBuildHelper",
                 "This will scan your scene for any Transforms sharing the same path and rename them to fix it.", "Continue", "Cancel");
 
             if (!doit) return;
+            
+            FixTransforms(Object.FindObjectsOfType<Transform>());
+        }
         
-            float progress = 0;
-            EditorUtility.DisplayProgressBar("Memes", "Doing the shit", progress);
+        [MenuItem("Tools/Build Helper/Fix scene sibling transforms", true)]
+        private static bool AllowSiblingFix()
+        {
+            return Selection.activeObject is GameObject && Selection.activeGameObject.scene.IsValid();
+        }
+        
+        [MenuItem("Tools/Build Helper/Fix sibling transforms for selected transform")]
+        public static void FixSiblingsForChildren()
+        {
+            bool doit = EditorUtility.DisplayDialog("VRBuildHelper",
+                "This will scan the selected objects' children for any objects sharing the same path and rename them to fix it.", "Continue", "Cancel");
 
-            GameObject[] GameObjectList = Object.FindObjectsOfType<GameObject>();
+            if (!doit) return;
+            
+            FixTransforms(Selection.activeGameObject.GetComponentsInChildren<Transform>().Where(t => t != Selection.activeTransform).ToArray());
+        }
+
+        private static void FixTransforms(Transform[] GameObjectList)
+        {
+            float progress;
             int goCount = GameObjectList.Length;
             int fixedCount = 0;
-        
+
             Scene currentScene = SceneManager.GetActiveScene();
             GameObject[] rootGameObjects = currentScene.GetRootGameObjects();
 
@@ -33,26 +52,27 @@ namespace BocuD.BuildHelper.Editor
             {
                 for (int index = 0; index < goCount; index++)
                 {
-                    GameObject go = GameObjectList[index];
-                
+                    Transform go = GameObjectList[index];
+
                     progress = (float)index / goCount;
                     EditorUtility.DisplayProgressBar("VRBuildHelper", "Fixing sibling transform names..", progress);
 
                     //handle scene root objects differently
                     if (go.transform.parent == null)
                     {
-                        List<GameObject> gos = rootGameObjects.Where(sibling => sibling != go && sibling.name == go.name).ToList();
+                        List<GameObject> gos = rootGameObjects.Where(sibling => sibling != go && sibling.name == go.name)
+                            .ToList();
                         for (int i = 0; i < gos.Count; i++)
                             gos[i].name = $"{gos[i].name}-{i:00}";
 
                         fixedCount += gos.Count;
                         continue;
                     }
-                
+
                     for (int idx = 0; idx < go.transform.parent.childCount; ++idx)
                     {
                         Transform transform = go.transform.parent.GetChild(idx);
-                        
+
                         //skip self
                         if (transform == go.transform)
                             continue;
